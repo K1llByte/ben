891f49895d8d62432c2ddc2760e186f166fe101a
kb <killbyte.prototype@gmail.com>
2022-10-03 02:02:07 +0100

    Hotfix

--- src/commands/minecraft.rs
+++ src/commands/minecraft.rs
@@ -4,14 +4,43 @@ use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{CommandResult, Args};
 
 #[group]
-#[commands(mc)]
+#[commands(mc,mcstart)]
 struct Minecraft;
 
 #[command]
 pub async fn mc(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     use std::process::Command;
-    if let Some(mc_cmd) = args.remains() {
+    if let Some(start) = args.current() {
+        if start == "start" {
+            // Start command
+            let res = Command::new("./run.sh")
+                .spawn();
+        
+            msg.reply(ctx, 
+                if let Ok(_) = res { format!("Success") }
+                else { "Service unavailable".to_string() }
+            ).await?;
+        }
+    }
+    else if let Some(mc_cmd) = args.remains() {
+        // Other commands
         let res = Command::new("./send.sh")
+            .arg(mc_cmd)
+            .spawn();
+        
+        msg.reply(ctx, 
+            if let Ok(_) = res { format!("Success") }
+            else { "Service unavailable".to_string() }
+        ).await?;
+    }
+    Ok(())
+}
+
+#[command]
+pub async fn mcstart(ctx: &Context, msg: &Message) -> CommandResult {
+    use std::process::Command;
+    if let Some(mc_cmd) = args.remains() {
+        let res = Command::new("./start.sh")
             .arg(mc_cmd)
             .spawn();
         
