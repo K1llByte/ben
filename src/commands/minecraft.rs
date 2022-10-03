8219a5568dd917cafddce23d89ce636a0ba8ceb8
kb <killbyte.prototype@gmail.com>
2022-10-03 02:16:45 +0100

    Hotfix

--- src/commands/minecraft.rs
+++ src/commands/minecraft.rs
@@ -9,29 +9,32 @@ struct Minecraft;
 
 #[command]
 pub async fn mc(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    use std::process::Command;
-    if let Some(start) = args.current() {
-        if start == "start" {
-            // Start command
-            let res = Command::new("./run.sh")
-                .spawn();
-        
-            msg.reply(ctx, 
-                if let Ok(_) = res { format!("Success") }
-                else { "Service unavailable".to_string() }
-            ).await?;
-        }
-        else {
-            if let Some(mc_cmd) = args.remains() {
-                // Other commands
-                let res = Command::new("./send.sh")
-                    .arg(mc_cmd)
+    // FIXME: Implement a proper permission system
+    if msg.author.id.0 == 181002804813496320u64 {
+        use std::process::Command;
+        if let Some(start) = args.current() {
+            if start == "start" {
+                // Start command
+                let res = Command::new("./run.sh")
                     .spawn();
-                
+            
                 msg.reply(ctx, 
                     if let Ok(_) = res { format!("Success") }
                     else { "Service unavailable".to_string() }
                 ).await?;
+            }
+            else {
+                if let Some(mc_cmd) = args.remains() {
+                    // Other commands
+                    let res = Command::new("./send.sh")
+                        .arg(mc_cmd)
+                        .spawn();
+                    
+                    msg.reply(ctx, 
+                        if let Ok(_) = res { format!("Success") }
+                        else { "Service unavailable".to_string() }
+                    ).await?;
+                }
             }
         }
     }
