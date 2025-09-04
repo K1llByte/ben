2cb54baffcde1d16acb75ae836645d46f681ef4d
kb <killbyte.prototype@gmail.com>
2025-09-04 02:36:49 +0100

    Update wm command to list contributors

--- src/commands/white_monster.rs
+++ src/commands/white_monster.rs
@@ -1,3 +1,4 @@
+use poise::serenity_prelude::UserId;
 use tracing::trace;
 
 use crate::{Context, Error};
@@ -6,30 +7,50 @@ use crate::{Context, Error};
 pub async fn wm(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
-    let wm_counter = ctx.data().wm_counter().await;
+    let wm_data = ctx.data().wm_counters().await;
 
-    ctx.say(format!("White monster cans: {} / 32 :white_monster:", wm_counter)).await?;
+    let mut output = format!("White monster cans: {} / 32\n", wm_data.0);
+    for (user_id, count) in wm_data.1 {
+        if let Some(cached_user) = ctx.cache().user(user_id) {
+            output.push_str(format!("- **{}** has {}\n",  cached_user.display_name(), count).as_str());
+        }
+        else {
+            let user = ctx.http().get_user(UserId::new(user_id)).await.unwrap();
+            output.push_str(format!("- **{}** has {}\n",  user.display_name(), count).as_str());
+        };
+    }
+    ctx.say(output).await?;
     Ok(())
 }
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn wmadd(
     ctx: Context<'_>,
+    #[description = "Number of white monsters to add (default: 1)"]
+    value: Option<u32>
 ) -> Result<(), Error> {
-    let wm_counter = ctx.data().inc_wm_counter().await;
+    let wm_counter = ctx.data().inc_wm_counter(
+        ctx.author().id.get(),
+        value.unwrap_or(1)
+    ).await;
 
     trace!("Incremented white monster counter to {}", wm_counter);
-    ctx.say(format!("Another white monster for the boysss! {} / 32 :white_monster:", wm_counter)).await?;
+    ctx.say(format!("Another white monster for the boysss! {} / 32", wm_counter)).await?;
     Ok(())
 }
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn wmrm(
     ctx: Context<'_>,
+    #[description = "Number of white monsters to remove (default: 1)"]
+    value: Option<u32>
 ) -> Result<(), Error> {
-    let wm_counter = ctx.data().dec_wm_counter().await;
+    let wm_counter = ctx.data().dec_wm_counter(
+        ctx.author().id.get(),
+        value.unwrap_or(1)
+    ).await;
 
     trace!("Decremented white monster counter to {}", wm_counter);
-    ctx.say(format!("Removed one white monster from the counter! {} / 32 :white_monster:", wm_counter)).await?;
+    ctx.say(format!("Removed one white monster from the counter! {} / 32", wm_counter)).await?;
     Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -16,49 +16,85 @@ impl Model {
         Self { db_pool }
     }
 
-    pub async fn wm_counter(&self) -> u32 {
+    pub async fn wm_counters(&self) -> (u32, Vec<(u64, u32)>) {
         // FIXME: Handle results properly
-        let wm_counter: i64 = sqlx::query_scalar(
+        let wm_data: Vec<(String, u32)> = sqlx::query_as(
             r#"
-            SELECT counter
+            SELECT *
             FROM white_monster_counter
             "#
         )
+        .fetch_all(&self.db_pool)
+        .await
+        .unwrap();
+
+        let total_count = wm_data.iter().map(|(_, count)| *count).sum();
+        let wm_data_converted = wm_data
+            .iter()
+            .map(|(user_id, count)| (user_id.parse::<u64>().unwrap(), *count))
+            .collect::<Vec<(u64, u32)>>();
+        (total_count, wm_data_converted)
+    }
+
+    pub async fn inc_wm_counter(&self, user_id: u64, amount: u32) -> u32 {
+        // FIXME: Handle results properly
+        
+        let wm_counter: i64 = sqlx::query_scalar(
+            r#"
+            INSERT INTO white_monster_counter(user_id, count)
+            VALUES($1, $2) ON CONFLICT(user_id) DO
+            UPDATE SET count = count + $2
+            RETURNING count;
+            "#
+        )
+        .bind(user_id.to_string())
+        .bind(amount)
         .fetch_one(&self.db_pool)
         .await
         .unwrap();
+        
+        // let wm_counter: i64 = sqlx::query_scalar(
+        //     r#"
+        //     UPDATE white_monster_counter
+        //     SET counter = counter + $1
+        //     RETURNING counter;
+        //     "#
+        // )
+        // .bind(amount)
+        // .fetch_one(&self.db_pool)
+        // .await
+        // .unwrap();
 
         u32::try_from(wm_counter).unwrap()
     }
 
-    pub async fn inc_wm_counter(&self) -> u32 {
+    pub async fn dec_wm_counter(&self, user_id: u64, amount: u32) -> u32 {
         // FIXME: Handle results properly
         let wm_counter: i64 = sqlx::query_scalar(
             r#"
-            UPDATE white_monster_counter
-            SET counter = counter + 1
-            RETURNING counter;
+            INSERT INTO white_monster_counter(user_id, count)
+            VALUES($1, $2) ON CONFLICT(user_id) DO
+            UPDATE SET count = count - $2
+            RETURNING count;
             "#
         )
+        .bind(user_id.to_string())
+        .bind(amount)
         .fetch_one(&self.db_pool)
         .await
         .unwrap();
-
-        u32::try_from(wm_counter).unwrap()
-    }
-
-    pub async fn dec_wm_counter(&self) -> u32 {
-        // FIXME: Handle results properly
-        let wm_counter: i64 = sqlx::query_scalar(
-            r#"
-            UPDATE white_monster_counter
-            SET counter = MAX(counter - 1, 0)
-            RETURNING counter;
-            "#
-        )
-        .fetch_one(&self.db_pool)
-        .await
-        .unwrap();
+        
+        // let wm_counter: i64 = sqlx::query_scalar(
+        //     r#"
+        //     UPDATE white_monster_counter
+        //     SET counter = MAX(counter - $1, 0)
+        //     RETURNING counter;
+        //     "#
+        // )
+        // .bind(amount)
+        // .fetch_one(&self.db_pool)
+        // .await
+        // .unwrap();
 
         u32::try_from(wm_counter).unwrap()
     }
